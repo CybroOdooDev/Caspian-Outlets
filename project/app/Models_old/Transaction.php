0725868b79d396b4a0528f943cfa6cfa97b20b3e
Dr. Ralph <rapheal.djane@akaditi.com>
2021-04-11 23:28:49 +0000

    Recent Update

---
+++ project/app/Models_old/Transaction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    protected $fillable = ['user_id', 'amount', 'exttrid', 'reference', 'service_id', 'ts', 'currency_code', 'currency_value', 'mode_of_payment', 'redirect_url', 'callback_url', 'landingPageURL', 'status', 'responseCode', 'responseMessage', 'transaction_id'];
+    public $timestamps = false;
+}
