bb8b824209c8188a4fe6d6e40d0e8d3f3be627f3
Dr. Ralph <rapheal.djane@akaditi.com>
2021-04-11 23:24:07 +0000

    Delete Everything

--- storage/framework/views/1e2fdd77469ddce0767db884e12554bcb4d00abc.php
+++
@@ -1,7 +0,0 @@
-<?php echo $__env->yieldContent('styles'); ?>
-
-<?php echo $__env->yieldContent('content'); ?>
-
-<script src="<?php echo e(asset('assets/admin/js/load.js')); ?>"></script>
-
-<?php echo $__env->yieldContent('scripts'); ?>
--- storage/framework/views/608e3f29287212aac08d0a2b0d0adceceecd69bc.php
+++
@@ -1,9 +0,0 @@
-<?php $__currentLoopData = $prods; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $prod): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-<div class="docname">
-	<a href="<?php echo e(route('front.product', $prod->slug)); ?>">
-		<?php echo strlen($prod->name) > 66 ? str_replace($slug,'<b>'.$slug.'</b>',substr($prod->name,0,66)).'...' :
-		str_replace($slug,'<b>'.$slug.'</b>',$prod->name); ?> - <?php echo e($prod->showPrice()); ?>
-
-	</a>
-</div>
-<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
