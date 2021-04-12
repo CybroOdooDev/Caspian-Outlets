40e2d933dcf657a5260cdb4a0ae76764176a954f
Sreerag E <44105061+cheer-g@users.noreply.github.com>
2021-04-12 11:31:22 +0530

    [ADD] Automatic attendance

--- pos_login_odoo/controllers/main.py
+++ pos_login_odoo/controllers/main.py
@@ -8,6 +8,7 @@ import datetime, time
 from odoo.addons.website.controllers.main import Website
 _logger = logging.getLogger(__name__)
 from werkzeug.utils import redirect
+import werkzeug.utils
 
 
 class PosWebsiteLogin(Website):
@@ -24,13 +25,19 @@ class PosWebsiteLogin(Website):
         print("Time : ", login_start)
         print("Time : ", login_end)
         print("Time : ", current_time)
-        print("Test 1 : ", login_start >= current_time)
-        print("Test 2 : ", login_end <= current_time)
+        print("Test 1 : ", login_start <= current_time)
+        print("Test 2 : ", login_end >= current_time)
         values = request.params.copy()
         session_id = request.env['pos.session'].sudo().search(
             [('state', '=', 'opened'), ('user_id', '=', request.uid)])
         if ((login_start <= current_time) or not login_start) and ((login_end >= current_time) or not login_end):
             if not redirect and request.params['login_success']:
+                employee = request.env['hr.employee'].sudo().search([('user_id', '=', request.uid)])
+                print("Employee : ", employee.id)
+                attendance_id = request.env['hr.attendance'].sudo().create({
+                    'employee_id': employee.id,
+                    'check_in': fields.Datetime.now()
+                })
                 if request.env['res.users'].browse(request.uid).has_group(
                         'base.group_user'):
                     if request.env['res.users'].browse(request.uid).pos_config:
@@ -48,10 +55,12 @@ class PosWebsiteLogin(Website):
                                  ('config_id', '=', config_id.id)])
                             if current_session_id:
                                 request.session.logout(keep_db=True)
+                                attendance_id.check_out = fields.Datetime.now()
                                 values[
                                     'error'] = "Another session is already opened for this point of sale!"
                                 return request.render('web.login', values)
                             else:
+
                                 config_id.open_session_cb()
                                 redirect = '/pos/web'
                     else:
@@ -76,5 +85,15 @@ class PosWebsiteLogin(Website):
     @http.route('/', type='http', auth='none')
     def direct_login(self):
         return redirect('/web/login')
+
+    @http.route('/web/session/logout', type='http', auth="user")
+    def logout(self, redirect='/web'):
+        employee = request.env['hr.employee'].sudo().search([('user_id', '=', request.uid)], limit=1)
+        print("employee ", employee.id, request.uid)
+        attendance_id = request.env['hr.attendance'].sudo().search([('employee_id', '=', employee.id)], limit=1)
+        print("Attendance ", attendance_id)
+        attendance_id.check_out = fields.Datetime.now()
+        request.session.logout(keep_db=True)
+        return werkzeug.utils.redirect(redirect, 303)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
