1deb640d33c19bc80b168788c684df5fd2ed3351
Sreerag E <44105061+cheer-g@users.noreply.github.com>
2021-04-19 14:41:41 +0530

    [ADD] Expense Approve

---
+++ caspian_outlets/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Caspian Outlets',
+    'version': '14.0.1.0.0',
+    'summary': """
+        Restricts Documents on Expense and POS appropriately
+    """,
+    'description': """Expense report can only approved by certain person above a threshold amount
+                      Only displays allocated POS Outlets  """,
+    'author': 'Odoo SA,Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['hr_expense', 'point_of_sale'],
+    'data': [
+        'views/hr_expense.xml',
+        'security/ir.model.access.csv',
+        'data/data.xml'
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'AGPL-3',
+}
---
+++ caspian_outlets/models/hr_expense.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class HrExpense(models.Model):
+    _name = 'hr.expense.limit'
+    _rec_name = 'auth_user_id'
+
+    auth_user_id = fields.Many2one('res.users', string="User")
+    threshold_amount = fields.Float(string="Threshold Amount")
+
+
+class ExpenseSheet(models.Model):
+    _inherit = 'hr.expense.sheet'
+
+    def _get_add_approve(self):
+        self.add_approve = False
+        limit = self.env['hr.expense.limit'].search([], limit=1)
+        current_uid = self.env.context.get('uid', False)
+        if self.total_amount >= limit.threshold_amount:
+            if current_uid == limit.auth_user_id.id:
+                self.add_approve = True
+
+    add_approve = fields.Boolean(compute=_get_add_approve)
