390533714191b38593fe7e4e47ee8039173b302b
CybroOdooDev <cybroodoo@gmail.com>
2021-05-12 17:34:18 +0530

    ADD Sale_pos report

---
+++ pos_sale_report/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "POS Sale Report",
+    'version': '14.0.1.0.0',
+    'summary': """Sale report  margin""",
+    'category': 'POS',
+    'depends': ['stock', 'point_of_sale'],
+    'data': [
+        'views/report.xml'
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
