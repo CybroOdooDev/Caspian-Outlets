5b12c22a7031bb41569b8ef280742bc790a12c39
COutlets <80832962+COutlets@users.noreply.github.com>
2021-06-16 17:20:52 +0530

    Update mail_notification.py

--- email_and_notification/models/mail_notification.py
+++ email_and_notification/models/mail_notification.py
@@ -8,64 +8,64 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
 
-    def _action_done(self):
-        res = super(StockPicking, self)._action_done()
-        if self.state is 'done':
-            group = self.env.ref('pos_disable_all.group_admin')
-            admins = []
-            admins_email = []
-            for user in group.users:
-                admins.append(user.partner_id.id)
-            for admin in admins:
-                user_id = self.env['res.partner'].browse(admin)
-                admins_email.append(user_id.email)
-            po_id = self.env['purchase.order'].search([('name', '=', self.origin)])
-            trans_ref_id = self.env['stock.picking'].search([('origin', '=', self.origin)])
-            trans_ref_list = []
-            for rec in trans_ref_id:
-                trans_ref_list.append(rec.name)
-            moves = []
-            demand = 0
-            total_demand = 0
-            total_received = 0
-            for line in po_id.order_line:
-                move = {
-                    'product': line.product_id.name,
-                    'demand_qty': line.product_qty,
-                    'done_qty': line.qty_received,
-                    'unit_price': line.price_unit,
-                    'price_subtotal': line.price_subtotal
-                }
-                total_demand += line.product_qty
-                total_received += line.qty_received
-                if line.qty_received != line.product_qty:
-                    demand += 1
-                moves.append(move)
-            for email in admins_email:
-                data = {
-                    'email': email,
-                    'from': self.env.user.email,
-                    'moves': moves,
-                    'po_id': self.origin,
-                    'validated': self.env.user.name,
-                    'vendor': self.partner_id.name,
-                    'deliver_to': str(
-                        self.location_dest_id.location_id.name) + "/" + str(
-                        self.location_dest_id.name),
-                    'done_date': self.date_done,
-                    'backorder': 'YES' if po_id.picking_count > 1 else 'NO',
-                    'trans_ref': (', '.join(trans_ref_list)),
-                    'demand': 'YES' if demand > 1 else 'NO',
-                    'total_demand': total_demand,
-                    'total_received': total_received
-                }
-                template = self.env.ref(
-                    'email_and_notification.mail_template_receive_product').sudo()
-                template.with_context(data).send_mail(
-                    self.id, force_send=True)
-            return res
-        else:
-            return res
+#     def _action_done(self):
+#         res = super(StockPicking, self)._action_done()
+#         if self.state is 'done':
+#             group = self.env.ref('pos_disable_all.group_admin')
+#             admins = []
+#             admins_email = []
+#             for user in group.users:
+#                 admins.append(user.partner_id.id)
+#             for admin in admins:
+#                 user_id = self.env['res.partner'].browse(admin)
+#                 admins_email.append(user_id.email)
+#             po_id = self.env['purchase.order'].search([('name', '=', self.origin)])
+#             trans_ref_id = self.env['stock.picking'].search([('origin', '=', self.origin)])
+#             trans_ref_list = []
+#             for rec in trans_ref_id:
+#                 trans_ref_list.append(rec.name)
+#             moves = []
+#             demand = 0
+#             total_demand = 0
+#             total_received = 0
+#             for line in po_id.order_line:
+#                 move = {
+#                     'product': line.product_id.name,
+#                     'demand_qty': line.product_qty,
+#                     'done_qty': line.qty_received,
+#                     'unit_price': line.price_unit,
+#                     'price_subtotal': line.price_subtotal
+#                 }
+#                 total_demand += line.product_qty
+#                 total_received += line.qty_received
+#                 if line.qty_received != line.product_qty:
+#                     demand += 1
+#                 moves.append(move)
+#             for email in admins_email:
+#                 data = {
+#                     'email': email,
+#                     'from': self.env.user.email,
+#                     'moves': moves,
+#                     'po_id': self.origin,
+#                     'validated': self.env.user.name,
+#                     'vendor': self.partner_id.name,
+#                     'deliver_to': str(
+#                         self.location_dest_id.location_id.name) + "/" + str(
+#                         self.location_dest_id.name),
+#                     'done_date': self.date_done,
+#                     'backorder': 'YES' if po_id.picking_count > 1 else 'NO',
+#                     'trans_ref': (', '.join(trans_ref_list)),
+#                     'demand': 'YES' if demand > 1 else 'NO',
+#                     'total_demand': total_demand,
+#                     'total_received': total_received
+#                 }
+#                 template = self.env.ref(
+#                     'email_and_notification.mail_template_receive_product').sudo()
+#                 template.with_context(data).send_mail(
+#                     self.id, force_send=True)
+#             return res
+#         else:
+#             return res
 
 
 class StockMoveLine(models.Model):
